c178f1a36bd6902c7297fc0f85e393f832d631fa
Carter Moyer <moyer6141@uwlax.edu>
2026-05-28 22:41:03 -0500

    feat(android): explicitly define androidClientId in capacitor configuration

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -8,6 +8,7 @@ const config: CapacitorConfig = {
     GoogleAuth: {
       scopes: ['profile', 'email'],
       serverClientId: '108585498879-v35d0d196d4djglodbbjok452pr1e04r.apps.googleusercontent.com',
+      androidClientId: '108585498879-o171u9a80ssqfkojpd8hohgq6dumk0iu.apps.googleusercontent.com',
       forceCodeForRefreshToken: true
     }
   }
